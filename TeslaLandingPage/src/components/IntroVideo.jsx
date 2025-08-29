4057f69314de66ac18f4cca39d8d1e31b853122c
prabhatsingh415 <prabhatsinghrajput795@gmail.com>
2025-08-29 20:16:12 +0530

    changed the favicon

--- TeslaLandingPage/src/components/IntroVideo.jsx
+++ TeslaLandingPage/src/components/IntroVideo.jsx
@@ -1,26 +1,32 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import introVideo from "../assets/videos/Intro.mp4";
 
 function IntroVideo() {
-    const [showIntro, setShowIntro] = useState(true);
+  const [showIntro, setShowIntro] = useState(true);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setShowIntro(false);
-        }, 4000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowIntro(false);
+    }, 4000);
 
-        return () => {
-            clearTimeout(timer)
-        }
-    }, [])
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
-    if (!showIntro) return null;
+  if (!showIntro) return null;
 
-    return (
-        <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden z-50 ">
-            <video src={introVideo} autoPlay muted className="w-full h-full object-cover"/>
-        </div>
-    );
+  return (
+    <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden z-50 ">
+      <video
+        src={introVideo}
+        autoPlay
+        preload="auto"
+        muted
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
 }
 
 export default IntroVideo;
